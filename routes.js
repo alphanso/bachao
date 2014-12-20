f01dc73ecaab239f9f717111e6c5b778a1b88f7f
Rishabh Singhal <rishabh@kahahai.in>
2014-12-20 12:53:24 +0530

    add home page and bootstrap3

---
+++ routes.js
@@ -0,0 +1,17 @@
+Router.map(function() {
+
+  this.route('home', {
+    path: '/'
+  });
+
+
+  this.route('notFound', {
+    path: '*',
+    where: 'server',
+    action: function() {
+      this.response.statusCode = 404;
+      this.response.end(Handlebars.templates['404']());
+    }
+  });
+
+});
